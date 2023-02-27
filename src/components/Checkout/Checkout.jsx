39848534f538e1bb4d728771edf2884f564bf00d
Abraham Medina <abrahamjosuemedinaarrocha@gmail.com>
2023-02-27 13:48:43 +0000

    Revisando error indexOf

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,8 +1,8 @@
 import { useCartContext } from "../../context/CartContext"
 import { Link } from "react-router-dom"
-import  React  from "react"
+import React, {useState} from "react"
 import { useNavigate } from "react-router-dom"
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
 import { createOrdenCompra, getProducto, updateProducto } from "../../firebase/firebase"
 
 export const Checkout = () => {
@@ -10,11 +10,22 @@ export const Checkout = () => {
     const datosFormulario = React.useRef()
     let navigate = useNavigate()
 
+    const [user, setUser] = useState({ nombre: '', telefono: '', correo: '', correo2: '', direccion: '' })
+
+    if (user.correo === user.correo2) {
+        //logica de la orden
+    } else {
+        alert('Los correos deben ser idénticos')
+    }
+
     const consultarFormulario = (e) => {
         e.preventDefault()
         const datForm = new FormData(datosFormulario.current)
         const cliente = Object.fromEntries(datForm)
-
+        setUser({
+            nombre: cliente.nombre,
+            telefono: cliente.telefono})
+            
         const aux = [...cart]
 
         aux.forEach(prodCarrito => {
@@ -27,9 +38,9 @@ export const Checkout = () => {
         createOrdenCompra(cliente, aux, totalPrice(), new Date().toISOString()).then(ordenCompra => {
             toast.success(`¡Gracias por su compra!. Id de compra: ${ordenCompra.id
                 } por un total de EUR ${new Intl.NumberFormat('de-DE').format(totalPrice())} fue realizada con exito`, {
-                    pauseOnHover: false,
-                    theme: "dark",
-                });
+                pauseOnHover: false,
+                theme: "dark",
+            });
             emptyCart()
             e.target.reset()
             navigate("/")
@@ -58,12 +69,16 @@ export const Checkout = () => {
                             <input type="email" className="form-control" name="correo" />
                         </div>
                         <div className="mb-3">
+                            <label htmlFor="exampleInputEmail1" className="form-label text-dark">Repetir correo electrónico:</label>
+                            <input type="email" className="form-control" name="correo2" />
+                        </div>
+                        <div className="mb-3">
                             <label htmlFor="exampleInputPassword1" className="form-label text-dark">Número telefónico:</label>
-                            <input type="number" className="form-control" name="teléfono" />
+                            <input type="number" className="form-control" name="telefono" />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="consulta" className="form-label text-dark">Dirección:</label>
-                            <textarea className="form-control" name="consulta" rows={3} defaultValue={""} />
+                            <textarea className="form-control" name="direccion" rows={3} defaultValue={""} />
                         </div>
                         <div className="d-flex justify-content-center">
                             <button type="submit" className="btn btn-dark" style={{ width: 500 }}>Finalizar compra</button>
